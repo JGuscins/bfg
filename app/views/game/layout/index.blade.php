0a6adf8949922feca649ebb478a1ada5fc0f691f
Jurij Guscins <jurijs.guscins@webembassy.com>
2014-01-18 19:26:45 +0200

    x

--- app/views/game/layout/index.blade.php
+++ app/views/game/layout/index.blade.php
@@ -11,9 +11,46 @@
     <![endif]-->
   </head>
   <body>
+    <div id="fb-root"></div>
     <div class="container">
       @yield('content')
     </div>
+
+    <button id="pay">Buy Product</button>
+    
+    <script type="text/javascript">
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '556335464450181',
+          status     : true,
+          cookie     : true,
+          xfbml      : true
+        });
+
+        function buy() {
+          var obj = {
+            method: 'pay',
+            action: 'purchaseitem',
+            product: 'YOUR_PRODUCT_URL'
+          };
+
+          FB.ui(obj, function(data) {
+              console.log(data);
+            });
+        }
+
+        document.getElementById('pay').onclick = function() {buy()};
+      };
+
+      // Load the SDK Asynchronously
+      (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+      }(document));
+    </script>
     <script src="//code.jquery.com/jquery.js"></script>
     <script src="{{ URL::to('packages/js/bootstrap.min.js') }}"></script>
   </body>
