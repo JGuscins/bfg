273bbdbf094493bb1ac3bd823ae54aba913007ee
Jurij Guscins <jurijs.guscins@webembassy.com>
2014-01-19 17:11:40 +0200

    x

--- public/js/game.js
+++ public/js/game.js
@@ -78,7 +78,7 @@ $(document).ready(function(e) {
 				myArray = myString.split(' ');
 
 	    		$('#q'+newkey).html(''+ myArray[0] +'<br>'+ myArray[1] +' <span><img class="profile-image" src="'+ value.picture +'"></span>');
-	    		$('#g'+newkey).data('id', newkey).data('uid', value.uid);
+	    		$('#g'+newkey).attr('data-id', newkey).attr('data-uid', value.uid);
 			});
 		});
 	}
