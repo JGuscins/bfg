c19e57b6f12eef2b6926e3f8310098842d96b731
Jurij Guscins <jurijs.guscins@webembassy.com>
2014-01-19 15:53:32 +0200

    x

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,21 @@
 $(document).ready(function(e) {
-	console.log('x');
+	// 50-50
+	$('#50-50').click(function(e) {
+		console.log('50-50');
+	});	
+
+	// STOP TIME
+	$('#stop-time').click(function(e) {
+		console.log('stop-time');
+	});	
+
+	// CHANGE QUESTION
+	$('#change-question').click(function(e) {
+		console.log('change-question');
+	});	
+
+	// SHOW ANSEER
+	$('#show-answer').click(function(e) {
+		console.log('show-answer');
+	});	
 });
