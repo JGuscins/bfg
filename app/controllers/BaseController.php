1d7bbc3d3ae5cfab8bbd3753b3dc55c6cdba7955
Jurij Guscins <jurijs.guscins@webembassy.com>
2014-01-19 12:47:30 +0200

    x

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -7,6 +7,27 @@ class BaseController extends Controller {
 	 *
 	 * @return void
 	 */
+
+	public static function search_image($url) {
+	    $crl = curl_init();
+
+	    curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+	    curl_setopt($crl, CURLOPT_URL, $url);
+	    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
+
+	    $ret = curl_exec($crl);
+	    curl_close($crl);
+
+	    $data = json_decode($ret);
+
+		foreach ($data->responseData->results as $key => $result) {
+			if($key == 0) {
+				return $result->url;
+			}
+		}
+	}
+
 	protected function setupLayout()
 	{
 		if ( ! is_null($this->layout))
