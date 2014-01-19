f39dbeff058fa98a7aa459c2df9c83b6e683454b
Jurij Guscins <jurijs.guscins@webembassy.com>
2014-01-19 12:28:44 +0200

    x

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -4,7 +4,18 @@ class TestController extends BaseController {
 
 	public function index()
 	{
-		return View::make('game.intro.test');
+$link = "http://images.google.com/images?q=flying&tbm=isch";
+$code = file_get_contents($link,'r');
+
+ereg ("imgurl=http://www.[A-Za-z0-9-]*.[A-Za-z]*[^.]*.[A-Za-z]*", $code, $img);
+ereg ("http://(.*)", $img[0], $img_pic);
+
+$firstImage = $img_pic[0];
+$firstImage = trim("$firstImage");
+echo "$firstImage<br><br>";
+
+// Display image
+echo "<img src=\"$firstImage\">";
 	}
 	
 	public function post() {
