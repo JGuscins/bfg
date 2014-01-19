72cd3941e4e0c138b460aef25c7eb206be7d3042
Jurij Guscins <jurijs.guscins@webembassy.com>
2014-01-19 12:40:48 +0200

    x

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -4,6 +4,19 @@ class TestController extends BaseController {
  
 	public function index()
 	{
+		function get_url_contents($url) {
+		    $crl = curl_init();
+
+		    curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+		    curl_setopt($crl, CURLOPT_URL, $url);
+		    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+		    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
+
+		    $ret = curl_exec($crl);
+		    curl_close($crl);
+		    return $ret;
+		}
+
 		$json = get_url_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=korn');
 
 		$data = json_decode($json);
@@ -12,7 +25,7 @@ class TestController extends BaseController {
 		    $results[] = array('url' => $result->url, 'alt' => $result->title);
 		}
 
-		dd($result);
+		dd($results);
 	}
 	
 	public function post() {
